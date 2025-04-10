5875ab8a10e3a4b7553f79b46da785707468b02b
Gurkan UCAR <ucar.gurkan@hotmail.com>
2025-04-10 21:08:24 +0300

    feat: create application common classes

---
+++ src/main/java/com/gucardev/walletappbackendspringboot/application/common/model/dto/BaseDto.java
@@ -0,0 +1,24 @@
+package com.gucardev.walletappbackendspringboot.application.common.model.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+public class BaseDto {
+
+    //    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
+    private LocalDateTime createdDate;
+
+    //    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
+    private LocalDateTime lastModifiedDate;
+
+    private String createdBy;
+
+    private String lastModifiedBy;
+
+}
